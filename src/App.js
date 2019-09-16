a6c9c1d7f3e8511e645120f0bd061282baf27f57
Rafael Amorim <hi@rafaismy.name>
2019-09-16 17:11:44 -0400

    basic game

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { SafeAreaView, Text } from 'react-native';
+import { mapping, light as lightTheme } from '@eva-design/eva';
+import { ApplicationProvider } from 'react-native-ui-kitten';
+import RootScreen from './Containers/Root/RootScreen';
 
 export default function App() {
   return (
-    <SafeAreaView>
-      <Text>
-        Hello world
-      </Text>
-    </SafeAreaView>
+    <ApplicationProvider mapping={mapping} theme={lightTheme}>
+      <RootScreen />
+    </ApplicationProvider>
   );
 }
