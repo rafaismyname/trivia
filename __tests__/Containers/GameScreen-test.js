730ad029995ee0eba757a2a102e2395351cd021f
Rafael Amorim <hi@rafaismy.name>
2019-09-20 17:01:30 -0400

    init gamescreen tests

---
+++ __tests__/Containers/GameScreen-test.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { GameScreen } from '../../src/Containers/Game/GameScreen';
+
+const initialProps = {
+  questionsSize: 1,
+  currentQuestionNumber: 1,
+  currentQuestion: {
+    category: 'Test Category',
+    question: 'Is this a test?',
+  },
+  currentAnswer: {},
+  chooseAnswer: jest.fn(),
+};
+
+const setup = (props = initialProps) => shallow(<GameScreen {...props} />);
+
+describe('Test GameScreen container', () => {
+  const wrapper = setup();
+
+  it('should render properly', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+});
