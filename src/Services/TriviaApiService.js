1f6289f4886b1b99467b45116dbe80eeb4ce45ef
Rafael Amorim <hi@rafaismy.name>
2019-10-03 15:58:14 -0400

    params for trivia api service

--- src/Services/TriviaApiService.js
+++ src/Services/TriviaApiService.js
@@ -1,22 +1,38 @@
 import { AllHtmlEntities } from 'html-entities';
 
-const fetchQuestions = () => {
-  const uri = 'https://opentdb.com/api.php?amount=10&difficulty=hard&type=boolean';
+const htmlEntities = new AllHtmlEntities();
+const htmlDecode = htmlEntities.decode;
+
+const parseQuestions = (question) => {
+  return {
+    category: htmlDecode(question.category),
+    question: htmlDecode(question.question),
+    difficulty: question.difficulty,
+    type: question.type,
+    answers: [question.correct_answer, ...question.incorrect_answers],
+    correctAnswer: question.correct_answer,
+  };
+};
+
+const fetchQuestions = (amount = 10, difficulty = 'hard', type = 'boolean') => {
+  const baseUri = 'https://opentdb.com/api.php';
+
+  const amountParam = `amount=${amount || 1}`;
+
+  const difficulties = ['easy', 'medium', 'hard'];
+  const parsedDifficulty = difficulty && difficulties.includes(difficulty) && difficulty;
+  const difficultyParam = parsedDifficulty ? `&difficulty=${parsedDifficulty}` : '';
+
+  const types = ['boolean', 'multiple'];
+  const parsedType = type && types.includes(type) && type;
+  const typeParam = parsedType ? `&type=${parsedType}` : '';
+
+  const uri = `${baseUri}?${amountParam}${difficultyParam}${typeParam}`;
 
   return fetch(uri)
     .then((response) => response.json())
     .then((response) => response.results)
-    .then((questions) => {
-      const entities = new AllHtmlEntities();
-      return questions.map((question) => ({
-        category: entities.decode(question.category),
-        question: entities.decode(question.question),
-        difficulty: question.difficulty,
-        type: question.type,
-        answers: [question.correct_answer, ...question.incorrect_answers],
-        correctAnswer: question.correct_answer,
-      }));
-    });
+    .then((questions) => questions.map(parseQuestions));
 };
 
 export default {
